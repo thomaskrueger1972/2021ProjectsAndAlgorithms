e8dc5de1a999cac0d1515687f881d0a9f946e41d
Thomas Krueger <66087507+thomaskrueger1972@users.noreply.github.com>
2021-03-31 15:51:41 -0400

    Add files via upload

---
+++ RecursionToDo3.js
@@ -0,0 +1,55 @@
+function rBinarySearch(arr, value) {
+    var mid = Math.floor(arr.length/2);
+    if(arr[mid] == value) {
+        return true;
+    }
+    else if(value < arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(0, mid), value);
+    }
+    else if(value > arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(mid, arr.length), value)
+    } else {
+        return false
+    }
+}
+
+console.log(rBinarySearch([1, 3, 5, 6], 4));
+console.log(rBinarySearch([4, 5, 6, 8, 12], 5));
+
+
+function rGCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        return rGCF(param1-param2, param2);
+    }
+    if (param1 < param2) {
+        return rGCF(param1, param2-param1);
+    }
+}
+
+console.log(rGCF(3, 7));   
+console.log(rGCF(18, 27)); 
+console.log(rGCF(49, 14)); 
+
+function GCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1-param2, param2);
+        }
+    }
+    if (param1 < param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1, param2-param1);
+        }
+    }
+}
+console.log(GCF(123456, 987654));
